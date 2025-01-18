ae461d1c005ff7346ca111df0afd7052de30d2c6
Jafar Isbarov <cefer.isbarov@gmail.com>
2025-01-18 21:15:28 +0400

    revise aggregate script to match fewshot pattern, see #1

--- scripts/aggregate.py
+++ scripts/aggregate.py
@@ -4,9 +4,25 @@ import json
 LANGUAGE = "uzbek"
 MODEL_NAME = "google/gemma-2-27b-it"
 
+keyword_dict = {"uzbek": "Javob", "crimean-tatar": "Cevap", "tatar": "Җавап"}
 
-def get_acc(data):
+
+def get_acc(data, language):
+    keyword = keyword_dict[language]
     for i in data:
+        if f"{keyword}: A" in i["output"]:
+            i["prediction"] = "A"
+            continue
+        elif f"{keyword}: B" in i["output"]:
+            i["prediction"] = "B"
+            continue
+        elif f"{keyword}: C" in i["output"]:
+            i["prediction"] = "C"
+            continue
+        elif f"{keyword}: D" in i["output"]:
+            i["prediction"] = "D"
+            continue
+
         if "A)" in i["output"]:
             i["prediction"] = "A"
         elif "B)" in i["output"]:
@@ -27,15 +43,10 @@ def get_acc(data):
 base_path = f"data/{LANGUAGE}/outputs/no_cot_instruct/{MODEL_NAME}/"
 
 for subject in os.listdir(base_path):
-    # path = f"outputs_shuffled/no_cot_instruct/{MODEL_NAME}/{subject}.json"
     path = base_path + subject
     if not os.path.exists(path):
         continue
 
     with open(path, "r") as f:
-        try:
-            data = json.load(f)
-        except Exception as e:
-            print(path)
-            print(e)
-        print(f"{subject[:-5]}: {100 * round(get_acc(data), 4)}")
+        data = json.load(f)
+        print(f"{subject[:-5]}: {100 * round(get_acc(data, LANGUAGE), 4)}")
