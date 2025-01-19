069b9ae7f8a812367be1613817b9d650c7fd77a5
Jafar Isbarov <cefer.isbarov@gmail.com>
2025-01-20 00:13:25 +0400

    add uyghur

---
+++ data/uyghur/create_json.py
@@ -0,0 +1,17 @@
+from datasets import load_dataset, Dataset
+
+dataset = load_dataset(
+    "jafarisbarov/UyghurMMLU", download_mode="force_redownload"
+)[
+    "train"
+]
+subjects = list(set(dataset["subject"]))
+
+for subject in list(set(subjects)):
+    subset = dataset.filter(lambda x: x["subject"] == subject)
+    Dataset.from_dict(subset[:5]).to_json(
+        f"./data/uyghur/dev/{subject}.jsonl", force_ascii=False
+    )
+    Dataset.from_dict(subset[5:]).to_json(
+        f"./data/uyghur/test/{subject}.jsonl", force_ascii=False
+    )
