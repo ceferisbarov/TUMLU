dbe9913dbcdb42fb248edeafb1b3170d8ab44c02
Jafar Isbarov <cefer.isbarov@gmail.com>
2025-01-19 13:40:58 +0400

    add kazakh dataset

---
+++ data/kazakh/create_json.py
@@ -0,0 +1,17 @@
+from datasets import load_dataset
+
+dataset = load_dataset(
+    "jafarisbarov/KazakhMMLU", download_mode="force_redownload"
+)
+
+subjects = list(set(dataset["test"]["subject"]))
+
+for subject in list(set(subjects)):
+    dev_subset = dataset["dev"].filter(lambda x: x["subject"] == subject)
+    test_subset = dataset["test"].filter(lambda x: x["subject"] == subject)
+    dev_subset.to_json(
+        f"./data/kazakh/dev/{subject}.jsonl", force_ascii=False
+    )
+    test_subset.to_json(
+        f"./data/kazakh/test/{subject}.jsonl", force_ascii=False
+    )
