a1a03c85b49ed12bbde73e5cbad14c7380618c75
Jafar Isbarov <cefer.isbarov@gmail.com>
2025-01-20 10:54:38 +0400

    add karakalpak dataset

---
+++ data/karakalpak/create_json.py
@@ -0,0 +1,16 @@
+from datasets import load_dataset, Dataset
+
+dataset = load_dataset(
+    "jafarisbarov/KarakalpakMMLU", download_mode="force_redownload"
+)["train"]
+
+subjects = list(set(dataset["subject"]))
+
+for subject in list(set(subjects)):
+    subset = dataset.filter(lambda x: x["subject"] == subject)
+    Dataset.from_dict(subset[:5]).to_json(
+        f"./data/karakalpak/dev/{subject}.jsonl", force_ascii=False
+    )
+    Dataset.from_dict(subset[5:]).to_json(
+        f"./data/karakalpak/test/{subject}.jsonl", force_ascii=False
+    )
