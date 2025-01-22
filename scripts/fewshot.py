6fc604946a878a55d2a52cb0a0ddb5f5a478332d
Jafar Isbarov <cefer.isbarov@gmail.com>
2025-01-22 23:33:29 +0400

    fix a typo

--- scripts/fewshot.py
+++ scripts/fewshot.py
@@ -23,7 +23,7 @@ MODEL_NAMES = [
     "Qwen/Qwen2.5-72B-Instruct",
     "meta-llama/Meta-Llama-3.1-8B-Instruct",
     "meta-llama/Meta-Llama-3.1-70B-Instruct",
-    "meta-llama/Llama-3.3-70B-Instruct"
+    "meta-llama/Llama-3.3-70B-Instruct",
     "gemini-1.5-flash",
     # "gemini-2.0-flash-exp", # very low rate limits, ignore
     "gemini-1.5-pro",
