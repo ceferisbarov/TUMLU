becb63d7795a96dab3d836fefc6c1dc6dfc62440
Jafar Isbarov <cefer.isbarov@gmail.com>
2025-01-29 21:32:48 +0400

    add cot script

---
+++ scripts/CoT.py
@@ -0,0 +1,173 @@
+import json
+import os
+from glob import glob
+from pathlib import Path
+
+from openai import OpenAI
+from anthropic import Anthropic, AnthropicVertex
+import google.generativeai as genai
+
+from tqdm import tqdm
+from dotenv import load_dotenv
+
+from utils import MODEL_NAMES, FEW_SHOT_PROMPTS, TEST_PROMPTS, format_fewshot_prompt, format_CoT_prompt, shuffle_choices
+
+load_dotenv(".env")
+
+METHOD="CoT" # or "fewshot"
+TEMPERATURE = 0.0
+MAX_TOKENS = 1024
+TOP_P = 1.0
+TEST_COUNT = 100
+LANGUAGE = "azerbaijani"
+SUBJECT_LIST_TEST = glob(f"data/{LANGUAGE}/test/*.jsonl")
+SUBJECT_DICT = {}
+
+for MODEL_NAME in MODEL_NAMES:
+    OUTPUT_PATH = f"data/{LANGUAGE}/outputs/cot_instruct/{MODEL_NAME}/"
+
+    if MODEL_NAME == "gpt-4o-2024-11-20":
+        client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
+    elif "claude" in MODEL_NAME:
+        client = Anthropic(api_key=os.environ["ANTHROPIC_API_KEY"])
+
+        # Uncomment to test claude@Vertex AI:
+        # project_id = "turkicmmlu"
+        # region = "europe-west1"
+        # client = AnthropicVertex(project_id=project_id, region=region)
+
+    elif "gemini" in MODEL_NAME:
+        genai.configure(api_key=os.environ["GEMINI_API_KEY"])
+    elif "qwen" in MODEL_NAME.lower() or "llama" in MODEL_NAME.lower():
+        client = OpenAI(
+            api_key=os.environ["DEEPINFRA_API_KEY"],
+            base_url="https://api.deepinfra.com/v1/openai",
+        )
+    else:
+        client = OpenAI(
+            api_key=os.environ["TOGETHER_API_KEY"],
+            base_url="https://api.together.xyz/v1",
+        )
+
+    for i in SUBJECT_LIST_TEST:
+        SUBJECT_DICT[Path(i).stem] = {}
+        SUBJECT_DICT[Path(i).stem]["path"] = i
+        dev_file_path = i.replace("test", "dev")
+        with open(dev_file_path, "r", encoding="utf8") as f:
+            json_content = []
+            for line in f:
+                json_content.append(json.loads(line))
+
+        str_content = ""
+        for j in json_content:
+
+            if METHOD == "CoT":
+                str_content = str_content + format_CoT_prompt(
+                    language=LANGUAGE,
+                    question=j["question"].strip(),
+                    choices=j["choices"],
+                    solution=j["CoT"],
+                )
+            else:
+                shuffled_choices, shuffled_answer = shuffle_choices(
+                    j["choices"], j["answer"]
+                )
+                str_content = str_content + format_fewshot_prompt(
+                    language=LANGUAGE,
+                    question=j["question"].strip(),
+                    choices=shuffled_choices,
+                    answer=shuffled_answer,
+                )
+        SUBJECT_DICT[Path(i).stem]["dev"] = str_content
+
+    problems = {}
+    for subject, content in SUBJECT_DICT.items():
+
+        output_file = OUTPUT_PATH + subject + ".json"
+        with open(content["path"], encoding="utf8") as f:
+            doc = []
+            for line in f:
+                doc.append(json.loads(line))
+        if os.path.exists(output_file):
+            print(output_file, "exists...")
+            continue
+        problem_indexes = []
+        output_dicts = []
+        system = content["dev"]
+        print("Subject: ", subject)
+        for q in tqdm(doc[:TEST_COUNT]):
+            shuffled_choices, shuffled_answer = shuffle_choices(
+                q["choices"], q["answer"]
+            )
+            assert (
+                len(shuffled_choices) <= 4
+            ), "The number of choices should not be more than 4!"
+            if METHOD == "CoT":
+                input = format_CoT_prompt(
+                    LANGUAGE, question=q["question"], choices=shuffled_choices
+                )
+            else:
+                input = format_fewshot_prompt(
+                    LANGUAGE, question=q["question"], choices=shuffled_choices
+                )
+            try:
+                if "claude" in MODEL_NAME:
+                    message = client.messages.create(
+                        model=MODEL_NAME,
+                        max_tokens=MAX_TOKENS,
+                        temperature=TEMPERATURE,
+                        system=system,
+                        messages=[
+                            {
+                                "role": "user",
+                                "content": [{"type": "text", "text": input}],
+                            },
+                        ],
+                    )
+                    output = message.content[0].text.strip()
+                elif "gemini" in MODEL_NAME:
+                    client = genai.GenerativeModel(
+                        MODEL_NAME, system_instruction=system
+                    )
+                    response = client.generate_content(
+                        input,
+                        generation_config=genai.GenerationConfig(
+                            max_output_tokens=MAX_TOKENS,
+                            temperature=TEMPERATURE,
+                        ),
+                    )
+                    output = response.text.strip()
+                else:
+                    response = client.chat.completions.create(
+                        model=MODEL_NAME,
+                        messages=[
+                            {"role": "system", "content": system},
+                            {"role": "user", "content": input},
+                        ],
+                        temperature=TEMPERATURE,
+                        max_tokens=MAX_TOKENS,
+                        top_p=TOP_P,
+                        stop=["\n\Savol:"],
+                    )
+                    output = response.choices[0].message.content.strip()
+
+            except Exception as e:
+                print(e)
+                problem_indexes.append(doc.index(q))
+                output = "[INVALID]"
+
+            output_dicts.append(
+                {
+                    "description": content["dev"],
+                    "question": q["question"],
+                    "choices": shuffled_choices,
+                    "output": output,
+                    "answer": shuffled_answer,
+                    "system": system,
+                    "input": input,
+                }
+            )
+        os.makedirs(os.path.dirname(OUTPUT_PATH), exist_ok=True)
+        problems[subject] = problem_indexes
+        with open(output_file, "w", encoding="utf8") as f:
+            json.dump(output_dicts, f, ensure_ascii=False, indent=4)
--- scripts/utils.py
+++ scripts/utils.py
@@ -33,6 +33,7 @@ MODEL_NAMES = [
 
 FEW_SHOT_PROMPTS = {
     "crimean-tatar": """Sual: {question}\n{choices}\n\nCevap: {answer}\n\n""",
+    "crimean-tatar-cyrillic": """Суаль: {question}\n{choices}\n\nДжевап: {answer}\n\n""",
     "uzbek": """Savol: {question}\n{choices}\n\nJavob: {answer}\n\n""",
     "tatar": """Сорау: {question}\n{choices}\n\nҖавап: {answer}\n\n""",
     "kazakh": """Сұрақ: {question}\n{choices}\n\nЖауап: {answer}\n\n""",
@@ -44,6 +45,7 @@ FEW_SHOT_PROMPTS = {
 
 TEST_PROMPTS = {
     "crimean-tatar": """Sual: {question}\n{choices}\n\nCevap: """,
+    "crimean-tatar-cyrillic": """Суаль: {question}\n{choices}\n\nДжевап: """,
     "uzbek": """Savol: {question}\n{choices}\n\nJavob: """,
     "tatar": """Сорау: {question}\n{choices}\n\nҖавап: """,
     "kazakh": """Сұрақ: {question}\n{choices}\n\nЖауап: """,
@@ -56,6 +58,7 @@ TEST_PROMPTS = {
 ANSWER_DICT = {
     "uzbek": "Javob",
     "crimean-tatar": "Cevap",
+    "crimean-tatar-cyrillic": "Джевап",
     "tatar": "Җавап",
     "kazakh": "Жауап",
     "karakalpak": "Juwap",
@@ -74,6 +77,48 @@ QUESTION_DICT = {
     "uyghur": "جاۋاب",
     "azerbaijani": "Sual",
 }
+
+SOLUTION_DICT = {
+    "uzbek": "yechim",
+    "crimean-tatar": "irinti",
+    "tatar": "чишелеш",
+    "kazak": "шешім",
+    "turkish": "çözüm",
+    "uyghur": "ھهل",
+    "azerbaijani": "həll"
+}
+
+
+def format_fewshot_prompt(language, question, choices, answer=None):
+    template = "{question_keyword.title()}: {question}\n{choices}\n{answer_keyword.title()}: {answer}"
+    template_no_answer = "{question_keyword.title()}: {question}\n{choices}\n{answer_keyword.title()}: "
+    choices_text = "\n".join(
+        f"{chr(65 + i)}) {choice}" for i, choice in enumerate(choices)
+    )
+    question_keyword = QUESTION_DICT[language]
+    answer_keyword = ANSWER_DICT[language]
+    if answer:
+        return template.format(question_keyword=question_keyword, question=question, choices=choices_text, answer_keyword=answer_keyword.title(), answer=answer)
+
+    return template_no_answer.format(question_keyword=question_keyword, question=question, choices=choices_text, answer_keyword=answer_keyword.title())
+
+def format_CoT_prompt(language, question, choices, solution=None):
+    """
+    Given a language specific template, question, choices,
+    format and return the template.
+    answer argument is necessary for FEW_SHOT_PROMPTS but not for TEST_PROMPTS.
+    """
+    template = "{question_keyword}: {question}\n{choices}\n{solution_keyword}: {solution}"
+    template_no_solution = "{question_keyword}: {question}\n{choices}\n{solution_keyword}: "
+    choices_text = "\n".join(
+        f"{chr(65 + i)}) {choice}" for i, choice in enumerate(choices)
+    )
+    question_keyword = QUESTION_DICT[language]
+    solution_keyword = SOLUTION_DICT[language]
+    if solution:
+        return template.format(question_keyword=question_keyword.title(), question=question, choices=choices_text, solution_keyword=solution_keyword.title(), solution=solution)
+
+    return template_no_solution.format(question_keyword=question_keyword.title(), question=question, choices=choices_text, solution_keyword=solution_keyword.title())
 
 def shuffle_choices(options, answer):
     """
@@ -122,13 +167,14 @@ def find_matching_pattern(text, language):
     Given an LLM output, matches it to A, B, C, or D.
     """
     text = text.replace("А", "A")
+    text = text.replace("В", "B")
     text = text.replace("Б", "B")
     text = text.replace("С", "C")
     text = text.replace("Д", "D")
 
     word = ANSWER_DICT[language]
     patterns = {
-        "A": [r"A\)", rf"{word}: A", rf"{word} A\)", r"\*\*A\*\*"],
+        "A": [r"A\)", rf"{word}: A", rf"{word.lower()}: A", rf"{word} A\)", rf"{word.lower()} A\)", r"\*\*A\*\*"],
         "B": [r"B\)", rf"{word}: B", rf"{word} B\)", r"\*\*B\*\*"],
         "C": [r"C\)", rf"{word}: C", rf"{word} C\)", r"\*\*C\*\*"],
         "D": [r"D\)", rf"{word}: D", rf"{word} D\)", r"\*\*D\*\*"],
@@ -142,14 +188,16 @@ def find_matching_pattern(text, language):
     return None
 
 
-def get_acc(data, language):
+def get_acc(data, language, normalize=False):
     """
     Calculates accuracy of an LLM on a given dataset.
     """
-    for i in data:
+    filtered_data = data
+    for i in filtered_data:
         i["prediction"] = find_matching_pattern(i["output"], language)
 
     if len(data):
-        return sum(map(lambda x: x["prediction"] == x["answer"], data)) / len(data)
-
+        if normalize:
+            return sum(map(lambda x: int(x["prediction"] == x["answer"]) * len(x["choices"]) / 4, filtered_data)) / len(filtered_data)
+        return sum(map(lambda x: int(x["prediction"] == x["answer"]), filtered_data)) / len(filtered_data)
     return 0
