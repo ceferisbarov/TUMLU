a6e737b8e593b332116ae484ea9f5e4ca77e3260
Jafar Isbarov <cefer.isbarov@gmail.com>
2025-02-16 02:40:45 +0400

    prune kazakh subset

--- data/kazakh/create_json.py
+++ data/kazakh/create_json.py
@@ -1,4 +1,4 @@
-from datasets import load_dataset
+from datasets import load_dataset, Dataset
 
 dataset = load_dataset("jafarisbarov/KazakhMMLU", download_mode="force_redownload")
 
@@ -7,5 +7,6 @@ subjects = list(set(dataset["test"]["subject"]))
 for subject in list(set(subjects)):
     dev_subset = dataset["dev"].filter(lambda x: x["subject"] == subject)
     test_subset = dataset["test"].filter(lambda x: x["subject"] == subject)
+    test_subset = Dataset.from_dict(test_subset[:100])
     dev_subset.to_json(f"./data/kazakh/dev/{subject}.jsonl", force_ascii=False)
     test_subset.to_json(f"./data/kazakh/test/{subject}.jsonl", force_ascii=False)
